984488f5a9475930a1acf4493ab87c12ada64495
Lee Keong <PreDated92@users.noreply.github.com>
2026-02-20 03:02:52 +0800

    Visual updates to Card. Cards will arrange side by side and can overlap.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './Card.css';
 
-// create card stack to hold and arrange these
-
 function Card(props) {  
   return (
-    <div className="business-card">
-      <h1>{props.name}</h1>
-      <p>{props.description}</p>
+    <div style={{
+      'background-color': `${props.description}`, 
+      'transform': `rotate(${props.rotate}deg)`}} 
+      // To fix: this transform prevents hover transform from working
+      className="business-card">
+        <h1>{props.name}</h1>
+        <p>{props.description}</p>
     </div>
   );
 }
